f32a5da87592352eec070e8a4cbffbb9ec6c2b71
Daniel Halperin <dhalperi@cs.washington.edu>
2013-12-02 09:27:42 -0800

    credentials: update for 4-partite keys as in Camille's script
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

--- code/cred.py
+++ code/cred.py
@@ -6,7 +6,7 @@ DEFAULT_FILE = os.path.join(os.path.dirname(__file__), 'credentials.cfg')
 def read_credentials(filename=DEFAULT_FILE):
     config = ConfigParser.ConfigParser()
     config.read(filename)
-    return dict(config.items('AccessToken'))
+    return dict(config.items('Twitter'))
 
 if __name__ == "__main__":
     test_file = os.path.join(os.path.dirname(__file__), \
