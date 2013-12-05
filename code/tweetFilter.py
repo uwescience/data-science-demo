1f2f7c98343fff0e24b524dbf689faf8d5e4fa05
Daniel Halperin <dhalperi@cs.washington.edu>
2013-12-04 17:08:59 -0800

    optimize and better explain tweetFilter.py
    See the comments in the TimedStreamListener
    
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

--- code/tweetFilter.py
+++ code/tweetFilter.py
@@ -43,22 +43,37 @@ ACCESS_TOKEN_SECRET = credentials['access_token_secret']
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-# Note: Had you wanted to perform the full OAuth dance instead of using
-# an access key and access secret, you could have uses the following 
-# four lines of code instead of the previous line that manually set the
-# access token via auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET).
-# 
-# auth_url = auth.get_authorization_url(signin_with_twitter=True)
-# webbrowser.open(auth_url)
-# verifier = raw_input('PIN: ').strip()
-# auth.get_access_token(verifier)
-
-## Note from CC - this might be the piece that is now deprecated.
-
 ## added by CC as a place to output tweets to a file, currently this file isn't ever closed
 outputfile = open('tweets.out', 'w')
 
 class TimedStreamListener(tweepy.StreamListener):
+    """A StreamListener that stops collecting data after a specified timeout. Here's the idea.
+
+    1) record the time the stream listener is created. This is the start time.
+
+    2) every time we get a new data packet, process it and then see if we've
+       reached the timeout. If so, return False to signal stop.
+
+    HOWEVER, this doesn't handle the case where no data for our search term
+    shows up once the timeout has been reached. The on_data function will never
+    be called. So, we ALSO use an HTTP timeout in the enclosing Stream object.
+    That timeout says "if the HTTP Stream has not received data for X seconds,
+    throw an exception."
+    
+    Note that if we use an HTTP timeout equal to the specified timeout, the
+    worst case is that we will actually wait 2*timeout seconds. Suppose we get
+    a data package at (start + timeout - epsilon). Then the on_data timeout
+    check will fail, so we won't stop. Then we wait another timeout seconds for
+    HTTP timeout, and fail. This brings us basically to start + 2*timeout.
+
+    To make a better approximation, use the HTTP timeout of something like 10%
+    of timeout. That way we can only be 10% off, i.e., wait 1.1 * timeout
+    instead of 2*timeout.
+
+    The code below also includes a min(1 second, timeout) minimum HTTP sleep.
+    In other words, we generally want the HTTP timeout to be at least 1 second
+    because otherwise we'll hit the on_timeout function repeatedly and will
+    keep killing the connection to Twitter and reconnecting."""
 
     def __init__(self, timeout=None):
         self.timeout = timeout
@@ -78,16 +93,16 @@ class TimedStreamListener(tweepy.StreamListener):
             return False
 
     def on_timeout(self):
-        print >> sys.stderr, 'stopping because no data for timeout...'
-        return False
+        if self.timeout and (time.time() - self.start) > self.timeout:
+            print >> sys.stderr, 'stopping because no data for timeout...'
+            return False
 
-# Create a streaming API and set a timeout value of 60 seconds.
-
-streaming_api = tweepy.streaming.Stream(auth, TimedStreamListener(timeout=args.timeout), timeout=args.timeout)
+# Create a streaming API and set a timeout value as described in TimedStreamListener above.
+http_timeout_cushion = min(1.5, args.timeout)
+http_timeout = max(http_timeout_cushion, args.timeout * 0.1)
+streaming_api = tweepy.streaming.Stream(auth, TimedStreamListener(timeout=args.timeout), timeout=http_timeout)
 
 # Optionally filter the statuses you want to track by providing a list
 # of users to "follow".
-
 print >> sys.stderr, 'Filtering the public timeline for "%s"' % (' '.join(sys.argv[1:]),)
-
 streaming_api.filter(follow=None, track=Q)
