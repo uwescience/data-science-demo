12cd59a141d8493a3e17d77006e432b71a5fc96b
Camille Cobb <cobbc12@gmail.com>
2013-12-05 00:44:58 -0800

    Removed the old twitterfight.py - will rename and replace.

--- code/twitterfight.py
+++ code/twitterfight.py
@@ -1,59 +1,83 @@
+import sys
+import tweepy
+import webbrowser
+import json 
+import time
+
+# Load credentials from credentials.cfg
+import cred
+credentials = cred.read_credentials()
+
+CONSUMER_KEY = credentials['consumer_key']
+CONSUMER_SECRET = credentials['consumer_secret']
+
+ACCESS_TOKEN = credentials['access_token_key']
+ACCESS_TOKEN_SECRET = credentials['access_token_secret'] 
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 DEFAULT_TIMEOUT = 10  # in seconds
 
-class TwitterFight:
-    
-    def __init__(self, searchArg1, searchArg2, timeout = DEFAULT_TIMEOUT):
-        self.result = [0, 0]
-        self.done = False
-        self.arg1 = searchArg1
-        self.arg2 = searchArg2
-        self.arg1_count = 0
-        self.arg2_count = 0
-        # start tweepy stream
-        # on status, figure out which searchArg it is, count it to that one
-        # wait 10 seconds
-        # end tweepy stream
-        # set self.done = True
-    
-    def result(self):
-        # could modify so that we always get most recent results so we can visualize
-        # to "watch" the bars on the graph go, more like a race
-        if self.done:
-            return self.result
-        return None
 
-class KeywordSearch:
-    def __init__(self, keywords):
-        self.keywords = keywords
-        # start tweepy filter stream
-        # write to file
+class TwoArgTimedStreamListener(tweepy.StreamListener):
+    
+    def __init__(self, arg1, arg2, score, timeout=None):
+        self.timeout = timeout
+        self.start = time.time()
+        self.arg1 = arg1
+        self.arg2 = arg2
+        self.score = score
+        tweepy.StreamListener.__init__(self)
+
+    def on_data(self, raw_json):
+        """This function is called whenever data (in the form of JSON) from
+        Twitter is received. Here we intercept the data and log it, then pass
+        it up to the super-class to be handled as usual.
+        """
+        data = json.loads(raw_json)
+        # print data['text']
+        if 'text' in data:
+            if self.arg1 in data['text']:
+                self.score[0] += 1
+                print 'arg1 ' + str(self.score[0])
+            if self.arg2 in data['text']:
+                self.score[1] += 1
+                print 'arg2 ' + str(self.score[1])
         
-    def mostRecent(self, num =1 ):
-        # return the last num tweets in the file
-        return None
-    
-    def allTweets(self):
-        # return all tweets so far
+        tweepy.StreamListener.on_data(self, raw_json)
+        if self.timeout and (time.time() - self.start) > self.timeout:
+            print >> sys.stderr, 'stopping because timeout reached...'
+            return False
+
+    def on_timeout(self):
+        if self.timeout and (time.time() - self.start) > self.timeout:
+            print >> sys.stderr, 'stopping because no data for timeout...'
+            return False
 
 
-'''
-These objects should contain whatever info is needed to plot on a map the right way.
-'''
-class GeoCoords:
-    def __init__(self, lat, longi):
-        self.lat = lat
-        self.longi = longi
 
-class GeoBounds:
-    def __init__(self, what, here):
-        self.dunno = dontknow
+class TwitterFight:
+ 
+    def __init__(self, searchArg1, searchArg2, timeout = DEFAULT_TIMEOUT):
+        self.searchArg1 = searchArg1
+        self.searchArg2 = searchArg2
+        self.timeout = timeout
+        self.Q = self.searchArg1 + ' ' + self.searchArg2
 
-# given tweet json, return coords
-def Geolocate(tweet_json):
-    # given tweet, if it has no location, return None
-    # if it has geocoordinates, return a GeoCoords object
-    # if it has geobounary, return a GeoBounds object
-    # if it has a region/city, return a corresponding city
+        self.score = [0,0]
 
-def 
+        http_timeout_cushion = min(1.5, self.timeout)
+        http_timeout = max(http_timeout_cushion, self.timeout * 0.1)
+
+        streaming_api = tweepy.streaming.Stream(auth, TwoArgTimedStreamListener(self.searchArg1, self.searchArg2, self.score, self.timeout), timeout=http_timeout)
+        streaming_api.filter(follow=None, track=self.Q)
+
+    def final_score(self):
+        return self.score
+
+
+f = TwitterFight('Seattle', 'Bieber', 30)
+print f.score
+print f.final_score()
+
